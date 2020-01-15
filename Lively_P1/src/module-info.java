49c4233771d2a73f18d73ff23dea7dd37ff8b8ce
Kyle <kyle@DEKSTOP-AB80TA6.com>
2020-01-15 12:15:50 -0500

    My first Commit

---
+++ Lively_P1/src/module-info.java
@@ -0,0 +1,2 @@
+module Lively_P1 {
+}
---
+++ My First Project/src/FirstProgram.java
@@ -0,0 +1,12 @@
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		int age = 12;
+		double weight = 123.345678;
+		
+		System.out.printf("Hello everyone!%nI am %d years old!%nI Weigh %.2f", age, weight);
+
+	}
+
+}
